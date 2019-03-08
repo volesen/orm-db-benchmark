488950be01baa9962786f52982a7b0c3b1b8d713
volesen <vincolesen@gmail.com>
2019-03-08 15:10:09 +0100

    A

--- flask/sqlachemy/app.py
+++ flask/sqlachemy/app.py
@@ -1,25 +1,68 @@
 import os
-from flask import Flask, request
+
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 from sqlalchemy_model import Base, Author, Album, Track
 
+
 app = Flask(__name__)
+app.config.update({
+    # os.environ.get('DB_URI'),
+    'SQLALCHEMY_DATABASE_URI': 'postgresql://root:password@localhost:5432/test',
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False
+})
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+
+# Initialize extensions
 db = SQLAlchemy(app, model_class=Base)
+ma = Marshmallow(app)
+
+# Define Marshmallow JSON serialization schema
 
 
-@app.route('/paginate/')
-def paginate_query():
-    p = request.args.get('p', 0)
-    
-    return p, 202
+class AuthorSchema(ma.ModelSchema):
+    class Meta:
+        model = Author
 
-@app.route('/serialize/')
-def serliaze_query():
-    n = request.args.get('n', 0)
-    return p, 202
+
+class AlbumSchema(ma.ModelSchema):
+    class Meta:
+        model = Album
+
+
+class TrackSchema(ma.ModelSchema):
+    class Meta:
+        model = Track
+
+
+author_schema = AuthorSchema()
+album_schema = AlbumSchema()
+track_schema = TrackSchema()
+
+# URL Endpoints
+@app.route('/serialize/<int:amount>')
+def serliaze_query(amount):
+    # Query given amount of objects to serialize
+    tracks = Track.query.limit(amount).all()
+
+    # Return serialized objects
+    serialized = track_schema.jsonify(tracks, many=True)
+
+    return serialized
+
+
+@app.route('/paginate/<int:page>')
+def paginate_query(page):
+
+    # Query objects by pagination
+    tracks = Track.query.paginate(page, per_page=10).items
+
+    # Return serialized objects
+    serialized = track_schema.jsonify(tracks, many=True)
+
+    return serialized
 
 
 if __name__ == '__main__':
